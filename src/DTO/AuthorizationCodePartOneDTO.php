48e41ff7214c19580069b406fabbed0a5166522a
maxim.martyskin <maxim.martyshkin@kartoteka.ru>
2023-01-07 08:57:16 +0300

    Добавил в DTO метод получения данных по code_chalenge для авторизации

--- src/DTO/AuthorizationCodePartOneDTO.php
+++ src/DTO/AuthorizationCodePartOneDTO.php
@@ -9,8 +9,6 @@ class AuthorizationCodePartOneDTO extends DataTransferObject
     /** @var string Тип гранта для предоставления токена учетных данных */
     public const GRANT_TYPE_RESPONSE_TYPE = 'code';
 
-// with the value code
-
     /** @var string Идентификатор клиента */
     public string $client_id;
 
@@ -22,4 +20,13 @@ class AuthorizationCodePartOneDTO extends DataTransferObject
 
     /** @var string|null Токен CSRF */
     public ?string $state = null;
+
+    /**
+     * Получение кода вызова
+     * @return string
+     */
+    public function codeChallenge(): string
+    {
+        return rtrim(strtr(base64_encode($this->client_id), '+/', '-_'), '=');
+    }
 }
