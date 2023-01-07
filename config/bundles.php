b013535cb8f80f248683f5791e09f18f7a7e01a2
maxim.martyskin <maxim.martyshkin@kartoteka.ru>
2023-01-07 09:13:05 +0300

    Добавил пакеты для создания миграций и создание uuid`а

--- config/bundles.php
+++ config/bundles.php
@@ -7,4 +7,5 @@ return [
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     League\Bundle\OAuth2ServerBundle\LeagueOAuth2ServerBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
 ];
